7093b78a12643571241f928937fc946b087a5c5a
jonagotthardt <jona.gotthardt@icloud.com>
2025-10-04 10:10:45 +0200

    consts.ts aktualisieren

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "CommunitySMP";
-export const SITE_DESCRIPTION = "Welcome to Website of CommunitySMP!";
+export const SITE_TITLE = "Jona Gotthardt";
+export const SITE_DESCRIPTION = "I am Jona Gotthardt. Download my Plugins and Mods!";
